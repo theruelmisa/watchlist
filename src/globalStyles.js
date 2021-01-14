e18ee853a814a5c2ad9382d0b17110ec1c17c990
Rumies <rumies.media@gmail.com>
2021-01-14 05:57:46 -0500

    initial commit

---
+++ src/globalStyles.js
@@ -0,0 +1,31 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    :root {
+
+    }
+
+    *, 
+    *::before,
+    *::after {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    html {
+        font-size: 62.5%; // 10px for easier rem sizing
+    }
+    body {
+        font-weight: 400;
+        line-height: 1.4;
+    }
+    a { 
+        color: inherit;
+        text-decoration: none;
+    }
+    li {
+        list-style: none;
+    }
+`;
+
+export default GlobalStyles;
